511f3d68fd71c6b2e49b0f117b30b6a34f261b74
indapa <indapa@gmail.com>
2026-08-28 19:27:09 +0000

    Refactor INTERSECT_REPEAT_CATALOG and JOIN_REPEAT_CATALOG_HITS processes for improved output handling

--- src/python/sv_trfcaller.py
+++ src/python/sv_trfcaller.py
@@ -1,18 +1,17 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
 # Usage: python sv_trfcaller.py <input.vcf> <output.tsv>
 # Usage with merged SV: python sv_trfcaller.py HPRC_SV.survivor.ins.vcf HPRC_SV.survivor.ins.trf.tsv
 # Usage with example merged SV: python sv_trfcaller.py ../../data/sv_output/survivor_multisample_vcf/first_500_INS.vcf ../../data/sv_output/survivor_multisample_vcf/first_500_INS.trf.tsv
 
-import sys
 import os
-
-import pytrf
-import parasail
-
-from cyvcf2 import VCF
-from tqdm   import tqdm
+import sys
 from contextlib import contextmanager
+
+import parasail
+import pytrf
+from cyvcf2 import VCF
+from tqdm import tqdm
 
 os.environ.setdefault("OPENBLAS_NUM_THREADS", "1")
 os.environ.setdefault("OMP_NUM_THREADS", "1")
@@ -20,12 +19,6 @@ os.environ.setdefault("MKL_NUM_THREADS", "1")
 
 matrix = parasail.matrix_create("ACGT", 2, -1)
 
-vcf     = VCF(sys.argv[1])
-out     = open(sys.argv[2], 'w')
-print('chrom', 'ins_coord', 'SVID', 'depth', 'insert_size', 'sample', 'rep_start', 'rep_end', 'motif', 'purity', 'motif_length', 'rep_length', 'rep_units', sep='\t', file=out)
-samples = vcf.samples
-
-sv_repeats = {}
 
 @contextmanager
 def suppress_pysam_output():
@@ -67,52 +60,93 @@ def suppress_pysam_output():
         os.close(save_stderr_fd)
 
 
-for variant in tqdm(vcf, ncols=80, smoothing=0.1, unit='variants'):
-    if variant.is_sv and variant.INFO.get("SVTYPE") == "INS":
-        ID    = variant.format('ID')
-        RAL   = variant.format('RAL')
-        AAL   = variant.format('AAL')
-        DR    = variant.format('DR')
-        LEN   = variant.format('LN')
+def main():
+    with VCF(sys.argv[1]) as vcf, open(sys.argv[2], "w") as out:
+        print(
+            "chrom",
+            "ins_coord",
+            "SVID",
+            "depth",
+            "insert_size",
+            "sample",
+            "rep_start",
+            "rep_end",
+            "motif",
+            "purity",
+            "motif_length",
+            "rep_length",
+            "rep_units",
+            sep="\t",
+            file=out,
+        )
+        samples = vcf.samples
 
-        for s, sample in enumerate(samples):
-            if ID[s] == 'NaN': continue
-            ALT = AAL[s][len(RAL):]
-            depth = DR[s]
-            insert_size = LEN[s]
+        for variant in tqdm(vcf, ncols=80, smoothing=0.1, unit="variants"):
+            if variant.is_sv and variant.INFO.get("SVTYPE") == "INS":
+                ID = variant.format("ID")
+                RAL = variant.format("RAL")
+                AAL = variant.format("AAL")
+                DR = variant.format("DR")
+                LEN = variant.format("LN")
 
-            n_trf = 0
-            trf_repeats    = []
+                for s, sample in enumerate(samples):
+                    if ID[s] == "NaN":
+                        continue
+                    ALT = AAL[s][len(RAL) :]
+                    depth = DR[s]
+                    insert_size = LEN[s]
 
-            with suppress_pysam_output():
-                for repeat in pytrf.ATRFinder(sample, ALT, min_motif=1, max_motif=100, min_identity=0.8):
-                    repeat    = repeat.as_string().split('\t')
-                    rep_start = int(repeat[1]) - 1
-                    rep_end   = int(repeat[2])
-                    motif     = repeat[3]
-                    motif_length = len(motif)
+                    trf_repeats = []
 
-                    rep_length = rep_end - rep_start
-                    # How many copies of the motif the repeat actually spans --
-                    # this is a reported column, and `novelty --min-rep-units`
-                    # thresholds on it.
-                    rep_units  = rep_length // motif_length
-                    # The alignment target is padded by two extra copies so a
-                    # semi-global alignment has somewhere to run off the end;
-                    # that padding must not leak into the reported copy number.
-                    query      = motif*(rep_units + 2)
+                    with suppress_pysam_output():
+                        for repeat in pytrf.ATRFinder(
+                            sample,
+                            ALT,
+                            min_motif=1,
+                            max_motif=100,
+                            min_identity=0.8,
+                        ):
+                            repeat = repeat.as_string().split("\t")
+                            rep_start = int(repeat[1]) - 1
+                            rep_end = int(repeat[2])
+                            motif = repeat[3]
+                            motif_length = len(motif)
 
-                    result = parasail.sg_trace_scan_16(ALT[rep_start:rep_end], query, 5, 1, matrix)
+                            rep_length = rep_end - rep_start
+                            # How many copies of the motif the repeat actually spans --
+                            # this is a reported column, and `novelty --min-rep-units`
+                            # thresholds on it.
+                            rep_units = rep_length // motif_length
+                            # The alignment target is padded by two extra copies so a
+                            # semi-global alignment has somewhere to run off the end;
+                            # that padding must not leak into the reported copy number.
+                            query = motif * (rep_units + 2)
 
-                    cigar_bytes  = result.cigar.decode
-                    cigar_str    = cigar_bytes.decode()
-                    cigar_simple = cigar_str.replace('=', 'M')
-                    purity       = float(repeat[15])/100
+                            parasail.sg_trace_scan_16(
+                                ALT[rep_start:rep_end], query, 5, 1, matrix
+                            )
 
-                    trf_repeats.append([variant.CHROM, variant.POS, ID[s], depth, insert_size, sample, rep_start, rep_end, motif, round(purity, 3), motif_length, rep_length, rep_units])
-                    n_trf += 1
+                            purity = float(repeat[15]) / 100
 
-            for rep in trf_repeats:
-                print(*rep, sep='\t', file=out)
+                            trf_repeats.append([
+                                variant.CHROM,
+                                variant.POS,
+                                ID[s],
+                                depth,
+                                insert_size,
+                                sample,
+                                rep_start,
+                                rep_end,
+                                motif,
+                                round(purity, 3),
+                                motif_length,
+                                rep_length,
+                                rep_units,
+                            ])
 
-out.close()
+                    for rep in trf_repeats:
+                        print(*rep, sep="\t", file=out)
+
+
+if __name__ == "__main__":
+    main()
